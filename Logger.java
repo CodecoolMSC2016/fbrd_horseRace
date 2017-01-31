91b3b1e5c6b854f78e95f0035244a5dea112114c
Bosnyák Imre Dávid <suti211@gmail.com>
2017-01-31 13:48:18 +0100

    Added .gitignore for .class files, implemented Date class into Logger

--- Logger.java
+++ Logger.java
@@ -1,5 +1,14 @@
-public class Logger{
-	public Logger(){
+import java.util.Date;
 
-	}	
+public class Logger{
+		
+	private Date date;	
+	
+	public Logger(){
+		date = new Date();
+	}
+
+	public void log(String type, String message){
+	
+	}
 }
