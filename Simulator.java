b942313670b0b91d858a1ed165b2672ae05d69f7
redgyuf <faramirex070@gmail.com>
2017-01-31 13:35:31 +0100

    created Simulator class, constructor

---
+++ Simulator.java
@@ -0,0 +1,11 @@
+public class Simulator{
+	private Simulation simulation;
+	private Logger logger;
+
+
+	public Simulator(Simulation simulation, Logger logger){
+		this.simulation = simulation;
+		this.logger = logger
+	}
+	
+}
