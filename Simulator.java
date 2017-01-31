16a737b002190bedf19cbd93510d45e6493de7cb
redgyuf <faramirex070@gmail.com>
2017-01-31 13:44:01 +0100

    added getters, setters

--- Simulator.java
+++ Simulator.java
@@ -5,7 +5,22 @@ public class Simulator{
 
 	public Simulator(Simulation simulation, Logger logger){
 		this.simulation = simulation;
-		this.logger = logger
+		this.logger = logger;
 	}
 	
+	public Simulation getSimulation(){
+		return simulation;
+	}
+	
+	public void setSimulation(Simulation simulation){
+		this.simulation = simulation;
+	}
+	
+	public Logger getLogger(){
+		return logger;
+	}
+	
+	public void setLogger(Logger logger){
+		this.logger = logger;
+	}
 }
