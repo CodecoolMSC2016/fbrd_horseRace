f5d84da5a5807d80f21b72999377242de3b72773
redgyuf <faramirex070@gmail.com>
2017-02-01 14:54:18 +0100

    Simulator class implemented

--- Simulator.java
+++ Simulator.java
@@ -1,11 +1,13 @@
 public class Simulator{
 	private Simulation simulation;
 	private Logger logger;
+	private int[] counterList = new int[8];
 
 
 	public Simulator(Simulation simulation, Logger logger){
 		this.simulation = simulation;
 		this.logger = logger;
+		
 	}
 	
 	public Simulation getSimulation(){
@@ -24,8 +26,38 @@ public class Simulator{
 		this.logger = logger;
 	}
 	
-	public Result run(){
+	public int findBestId(){		
+		for(int id : simulation.getWinnerIds()){
+			counterList[id] += 1;
+		}
 		
+		int bestId = 0;
+		
+		for(int i = 0; i < counterList.length; i++){
+			if(counterList[bestId] < counterList[i]){
+				bestId = i;
+			}
+		}
+		
+		return bestId;
+	}
+	
+	public double countWinningChance(){
+		
+		int maxi = 0;
+		for(int i = 0; i < counterList.length; i++){
+			if(counterList[maxi] < counterList[i]){
+				maxi = i;
+			}
+		}
+		int numOfWinOfTheBest = counterList[maxi];
+		
+		return numOfWinOfTheBest / counterList.length;
+	}
+	
+	public Result run(){	
+		System.out.println(countWinningChance());
+		Result result = new Result(findBestId(), countWinningChance());
 		return result;
 	}
 }
