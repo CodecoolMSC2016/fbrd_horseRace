aaf0a2b4c1805545960cb3028ada18ae4d0a29dd
Bosnyák Imre Dávid <suti211@gmail.com>
2017-02-01 13:50:21 +0100

    Revamped Simulation class

--- Simulation.java
+++ Simulation.java
@@ -1,32 +1,45 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.Math;
 import java.util.concurrent.ThreadLocalRandom;
-import java.io.Writer;
-import java.io.OutputStreamWriter;
-import java.io.FileOutputStream;
+
 public class Simulation {
+
+	private String filePath;
+	private Horse[] horses;
+	private int rounds;
+	private int[] winnerIds;
 	
-	public void simulateRace(Horse[] horses) {
+	public Simulation(Horse[] horses, int rounds){
+		this.horses = horses;
+		this.rounds = rounds;
+		filePath = "simulatedRuns.csv";
+		winnerIds = new int[rounds];
+	}	
+	
+	public void simulateRace() {
 		int id;
-		id = ThreadLocalRandom.current().nextInt(0, 7 + 1);
-		horses[id].increaseWins(1);
-		generateData(id, horses[id].getName(), horses[id].getWins());
-	}
-	
-	
-	public void generateData(int id, String name, int wins) {
-		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("simulatedRuns.csv"), "utf-8"))) {
-			writer.write(id + ";" + name + ";" + wins + "\n");
-			
+		for(int i = 0; i < rounds; i++){
+			id = ThreadLocalRandom.current().nextInt(0, 7 + 1);
+			try{
+				generateData(id, horses[id].getName());
+			}catch(IOException e)
+			{
+				e.printStackTrace();
+			}
 		}
-		catch (IOException e){
-		e.printStackTrace();
-		}
+		
 	}
-	
+
+	public void generateData(int id, String name) throws IOException{
+		FileWriter fw = new FileWriter(filePath, true);
+		fw.append(id + ";" + name + "\n");
+		fw.flush();
+		fw.close();
+	}
+
+	public int[] load(){
+		return winnerIds;
+	}
 	
 }
