1012c2779026d6f1d2b10a60e172cc9a087b68b7
Bosnyák Imre Dávid <suti211@gmail.com>
2017-02-02 14:17:54 +0100

    Implemented file prediction possibility in Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main{	
 	
 	static String[] horseNames = new String[]{"Kábó", "Kese", "Húskloffo-ló", "Void", "Emmavaccon", 
@@ -10,7 +12,10 @@ public class Main{
 		Logger logger = new Logger();
 		boolean shouldDump = false;
 		Result result;
-		
+		Result fileResult;
+		Scanner sc = new Scanner(System.in);
+		String input;
+
 		if(args.length > 0){
 			if(args[0].equalsIgnoreCase("-d")){
 				shouldDump = true;
@@ -48,12 +53,25 @@ public class Main{
 		if(shouldDump){
 			logger.dumpArray(sim.getWinnerIds());
 		}
-		
+
 		result = simltr.run();
 		logger.log("<RESULT>: ", "You should bet on " + racers[result.getId()].getName() +
 				 " he has " + result.getWinningChance() * 100 + "%" + " chance to win.\n");
+		//YOLO
+		logger.log("<INFO>: ", "Would you like to get a prediction based on simulation history?(y/n):\n");
+		input = sc.next();
 
-
+		if(input.equalsIgnoreCase("y")){
+			HistoryReader hr = new HistoryReader(racers);
+			Simulation simFile = hr.restoreSim();
+			Simulator simulateFromFile = new Simulator(simFile, logger);
+			fileResult = simulateFromFile.run();
+			
+			logger.dumpArray(simFile.getWinnerIds());
+			logger.log("<RESULT>: ", "You should bet on " + racers[fileResult.getId()].getName() +
+				 " he has " + fileResult.getWinningChance() * 100 + "%" + " chance to win.\n");
+		}
+		
 	}	
 	
 	public static Simulation generateSimulation(int rounds){
